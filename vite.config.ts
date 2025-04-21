3c228bec6c7dbbe31bb6cf74dc7b8d06bb740fc5
ShinMinkyu-Lily <67412396+ShinMinkyu-Lily@users.noreply.github.com>
2025-04-21 19:20:33 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'firebase/app':
-        fileURLToPath(new URL('node_modules/firebase/app/dist/index.esm.js', import.meta.url)),
-      'firebase/firestore':
-        fileURLToPath(new URL('node_modules/firebase/firestore/dist/index.esm.js', import.meta.url)),
-      'firebase/auth':
-        fileURLToPath(new URL('node_modules/firebase/auth/dist/index.esm.js', import.meta.url)),
-      'firebase/analytics':
-        fileURLToPath(new URL('node_modules/firebase/analytics/dist/index.esm.js', import.meta.url)),
+      // firebase core
+      'firebase/app': path.resolve(
+        __dirname,
+        'node_modules/firebase/app/dist/esm/index.js'
+      ),
+      // full SDK firestore
+      'firebase/firestore': path.resolve(
+        __dirname,
+        'node_modules/firebase/firestore/dist/index.mjs'
+      ),
+      // auth, analytics 등도 필요하다면 함께 설정
+      'firebase/auth': path.resolve(
+        __dirname,
+        'node_modules/firebase/auth/dist/index.mjs'
+      ),
+      'firebase/analytics': path.resolve(
+        __dirname,
+        'node_modules/firebase/analytics/dist/index.esm.js'
+      )
     }
   }
-})
+});
