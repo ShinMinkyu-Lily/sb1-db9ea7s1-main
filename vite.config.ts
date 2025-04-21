5e1fa0066159239a4366292257a0421bdb01be57
ShinMinkyu-Lily <67412396+ShinMinkyu-Lily@users.noreply.github.com>
2025-04-21 22:52:16 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'firebase/app': path.resolve(__dirname, 'node_modules/firebase/app/dist/esm/index.esm.js'),
-      'firebase/auth': path.resolve(__dirname, 'node_modules/firebase/auth/dist/esm/index.esm.js'),
-      'firebase/firestore': path.resolve(__dirname, 'node_modules/firebase/firestore/dist/esm/index.esm.js'),
-      'firebase/analytics': path.resolve(__dirname, 'node_modules/firebase/analytics/dist/esm/index.esm.js'),
+  build: {
+    rollupOptions: {
+      external: [
+        'firebase/app',
+        'firebase/auth',
+        'firebase/firestore',
+        'firebase/analytics',
+      ],
     },
   },
   optimizeDeps: {
-    include: [
-      'firebase/app',
-      'firebase/auth',
-      'firebase/firestore',
-      'firebase/analytics',
-    ],
+    exclude: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/analytics'],
   },
 });
