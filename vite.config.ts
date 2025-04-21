76da134822371c200a0cc8dc950a0ef9beb2b4ea
ShinMinkyu-Lily <67412396+ShinMinkyu-Lily@users.noreply.github.com>
2025-04-21 22:48:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,24 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path'; // 더 이상 필요 없을 수 있습니다.
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     // firebase/app 은 이 경로에 있습니다
-  //     'firebase/app':       path.resolve(__dirname, 'node_modules/firebase/app/dist/esm/index.esm.js'),
-  //     // firebase/auth
-  //     'firebase/auth':      path.resolve(__dirname, 'node_modules/firebase/auth/dist/esm/index.esm.js'),
-  //     // firebase/firestore
-  //     'firebase/firestore': path.resolve(__dirname, 'node_modules/firebase/firestore/dist/esm/index.esm.js'),
-  //     // firebase/analytics
-  //     'firebase/analytics': path.resolve(__dirname, 'node_modules/firebase/analytics/dist/esm/index.esm.js'),
-  //   }
-  // },
+  resolve: {
+    alias: {
+      'firebase/app': path.resolve(__dirname, 'node_modules/firebase/app'),
+      'firebase/auth': path.resolve(__dirname, 'node_modules/firebase/auth'),
+      'firebase/firestore': path.resolve(__dirname, 'node_modules/firebase/firestore'),
+      'firebase/analytics': path.resolve(__dirname, 'node_modules/firebase/analytics'),
+    },
+  },
   optimizeDeps: {
     include: [
       'firebase/app',
       'firebase/auth',
       'firebase/firestore',
-      'firebase/analytics'
-    ]
-  }
-})
+      'firebase/analytics',
+    ],
+  },
+});
