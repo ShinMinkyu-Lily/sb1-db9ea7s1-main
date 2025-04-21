5b4fbdff59488e9ef19f42879e6b5790f52d5429
ShinMinkyu-Lily <67412396+ShinMinkyu-Lily@users.noreply.github.com>
2025-04-21 18:57:49 +0900

    의존성 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: [
+      'firebase/app',
+      'firebase/firestore',
+      'firebase/auth',
+      'firebase/analytics'
+    ]
   },
-  build: {
-    rollupOptions: {
-      external: [
-        'firebase/app',
-        'firebase/firestore',
-        'firebase/auth',
-        'firebase/analytics'
-      ]
-    }
-  }
 });
