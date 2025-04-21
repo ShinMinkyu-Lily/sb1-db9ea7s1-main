e59bd376df0adb95c3f6c7dc2ac0ec2ba1779877
ShinMinkyu-Lily <67412396+ShinMinkyu-Lily@users.noreply.github.com>
2025-04-21 19:05:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    include: [
-      'firebase/app',
-      'firebase/firestore',
-      'firebase/auth',
-      'firebase/analytics'
-    ]
-  },
-});
+  resolve: {
+    alias: {
+      'firebase/app':
+        fileURLToPath(new URL('node_modules/firebase/app/dist/index.esm.js', import.meta.url)),
+      'firebase/firestore':
+        fileURLToPath(new URL('node_modules/firebase/firestore/dist/index.esm.js', import.meta.url)),
+      'firebase/auth':
+        fileURLToPath(new URL('node_modules/firebase/auth/dist/index.esm.js', import.meta.url)),
+      'firebase/analytics':
+        fileURLToPath(new URL('node_modules/firebase/analytics/dist/index.esm.js', import.meta.url)),
+    }
+  }
+})
